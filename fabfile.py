9c67ea38a09f345dfb57d269e17286d8f7478769
Daniel Friesel <derf@chaosdorf.de>
2011-04-06 18:07:22 +0200

    fabfile: Various improvements

--- fabfile.py
+++ fabfile.py
@@ -8,12 +8,19 @@ env.hosts = [
 	'root@vm.chaosdorf.de',
 ]
 
-def deploy(version):
-	put("../chaosdorf-admin-toolkit_%s_all.deb" % version, '/tmp/')
+env.shell = '/bin/sh -c'
+
+def check_etc():
 	run('etckeeper pre-install')
+
+def deploy(version):
+	check_etc()
+	put("../chaosdorf-admin-toolkit_%s_all.deb" % version, '/tmp/')
 	run("dpkg --install /tmp/chaosdorf-admin-toolkit_%s_all.deb" % version)
 	run('etckeeper post-install')
 	run("rm /tmp/chaosdorf-admin-toolkit_%s_all.deb" % version)
 
 def configs():
+	check_etc()
 	put('etckeeper/etckeeper.conf', '/etc/etckeeper/')
+	run('etckeeper commit "chaosdorf-admin-toolkit configfile updates"')
