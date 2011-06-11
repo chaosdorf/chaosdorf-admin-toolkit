66750a5a5bdbf480136aab7aae9c0b8389ae14a2
Daniel Friesel <derf@chaosdorf.de>
2011-06-11 08:26:39 +0200

    cron: Ignore nagios/run_checks output

--- debian/chaosdorf-admin-toolkit.cron.d
+++ debian/chaosdorf-admin-toolkit.cron.d
@@ -3,8 +3,8 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 MAILTO=root
 
-0,5,10,20,25,30,35,40,45,50,55 * * * * nagios /usr/lib/nagios/run_checks
-15 * * * * nagios /usr/lib/nagios/run_checks 60
+0,5,10,20,25,30,35,40,45,50,55 * * * * nagios /usr/lib/nagios/run_checks 2> /dev/null
+15 * * * * nagios /usr/lib/nagios/run_checks 60 2> /dev/null
 
 03 1 * * 1 root /usr/sbin/backup_external
 
