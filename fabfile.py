1da5f047c22d20b7c8b4c005efbb1496c51ecce5
Maximilian Gaß <mxey@mxey.net>
2013-05-23 23:16:21 +0200

    Tabs → Spaces

--- fabfile.py
+++ fabfile.py
@@ -4,11 +4,11 @@ import os
 from StringIO import StringIO
 
 env.hosts = [
-	'backend.chaosdorf.de',
-	'extern.chaosdorf.de',
-	'intern.chaosdorf.de',
-	'shells.chaosdorf.de',
-	# 'vm.chaosdorf.de',
+    'backend.chaosdorf.de',
+    'extern.chaosdorf.de',
+    'intern.chaosdorf.de',
+    'shells.chaosdorf.de',
+    # 'vm.chaosdorf.de',
 ]
 
 if os.getenv('USER') != 'mxey':
@@ -17,26 +17,26 @@ if os.getenv('USER') != 'mxey':
 env.shell = '/bin/sh -c'
 
 def etckeeper_check():
-	sudo('etckeeper pre-install')
+    sudo('etckeeper pre-install')
 
 def etckeeper_commit(message):
-	sudo('if etckeeper unclean; then etckeeper commit "%s"; fi' % message)
+    sudo('if etckeeper unclean; then etckeeper commit "%s"; fi' % message)
 
 def etckeeper_done():
-	sudo('etckeeper post-install')
+    sudo('etckeeper post-install')
 
 def configs():  
-  	etckeeper_check()
-  	put('etckeeper/etckeeper.conf', '/etc/etckeeper/', use_sudo=True)
-  	put('apt/99checkrestart', '/etc/apt/apt.conf.d/', use_sudo=True)
-	etckeeper_commit('chaosdorf-admin-toolkit configfile updates')
+    etckeeper_check()
+    put('etckeeper/etckeeper.conf', '/etc/etckeeper/', use_sudo=True)
+    put('apt/99checkrestart', '/etc/apt/apt.conf.d/', use_sudo=True)
+    etckeeper_commit('chaosdorf-admin-toolkit configfile updates')
  
 def deploy(version):
-	etckeeper_check()
-	put("../chaosdorf-admin-toolkit_%s_all.deb" % version, '/root/', use_sudo=True)
-	sudo("dpkg --install /root/chaosdorf-admin-toolkit_%s_all.deb" % version)
-	sudo("rm /root/chaosdorf-admin-toolkit_%s_all.deb" % version)
-	etckeeper_done()
+    etckeeper_check()
+    put("../chaosdorf-admin-toolkit_%s_all.deb" % version, '/root/', use_sudo=True)
+    sudo("dpkg --install /root/chaosdorf-admin-toolkit_%s_all.deb" % version)
+    sudo("rm /root/chaosdorf-admin-toolkit_%s_all.deb" % version)
+    etckeeper_done()
 
 def test():
     sudo("uptime")
@@ -90,11 +90,11 @@ sshPublicKey: %s
 # most munin plugins need to be edited and tested on figurehead
 @hosts('root@chaosdorf.dyndns.org')
 def get_munin_plugins():
-	with cd('/usr/share/munin/plugins/'):
-		get('freifunk_nodes', 'munin/')
-		get('modem_status', 'munin/')
-		get('online_ips', 'munin/')
-		get('tc_new', 'munin/')
+    with cd('/usr/share/munin/plugins/'):
+        get('freifunk_nodes', 'munin/')
+        get('modem_status', 'munin/')
+        get('online_ips', 'munin/')
+        get('tc_new', 'munin/')
 
 @hosts('extern.chaosdorf.de')
 def upgrade_mediawiki(version):
