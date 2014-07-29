917e8b6f13c33f46c878f28f5a36e3f1e98f7e35
Daniel Friesel <derf@finalrewind.org>
2014-07-29 13:05:49 +0200

    add chaosdorf-raumadmin-toolkit target

---
+++ debian/chaosdorf-raumadmin-toolkit.cron.d
@@ -0,0 +1,10 @@
+# /etc/cron.d/nagios-run-checks
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+MAILTO=root
+
+0,5,10,20,25,30,35,40,45,50,55 * * * * nagios /usr/lib/nagios/run_checks 2> /dev/null
+15 * * * * nagios /usr/lib/nagios/run_checks 60 2> /dev/null
+
+03 0 * * 1 root /usr/sbin/backup_external
+
