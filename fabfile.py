281ea622f810c92b97c14cce195f0b4eea074817
Daniel Friesel <derf@finalrewind.org>
2020-03-01 12:16:46 +0100

    port fabfile to Fabric 2
    Squashed commit of the following:
    
    commit e1f1f8c4a975d6fa4ebcd655350a1a6854a0d60d
    Author: Daniel Friesel <derf@finalrewind.org>
    Date:   Sun Mar 1 12:15:36 2020 +0100
    
        add jabber-adduser command
    
    commit dbcb76c4800cc26ac02c7d678c58a26f1a63106b
    Author: Daniel Friesel <derf@finalrewind.org>
    Date:   Sun Mar 1 11:59:39 2020 +0100
    
        add deluser wrapper command
    
    commit 8f5e0551bdbd059f9c1db0bd3302b772bd0cdc13
    Author: Daniel Friesel <derf@finalrewind.org>
    Date:   Sun Mar 1 11:54:38 2020 +0100
    
        add cgit and adduser tasks
    
    commit 2ddc4dd585f9febce86f1780ab076dfcfead89d4
    Author: Daniel Friesel <derf@finalrewind.org>
    Date:   Sat Feb 29 11:46:18 2020 +0100
    
        Restore cgit command
    
    commit 2c708a78fe29ed3d18364762b1924827f72256a7
    Author: Daniel Friesel <derf@finalrewind.org>
    Date:   Thu Feb 27 22:00:40 2020 +0100
    
        start fabric2 rewrite. so far, only account deletion is supported

--- fabfile.py
+++ fabfile.py
@@ -1,172 +1,114 @@
-from __future__ import with_statement
-from fabric.api import *
-from fabric.contrib.files import exists
+from fabric import task
+from fabric.connection import Connection
+import io
 
-import os
-from StringIO import StringIO
+all_hosts=list(map(lambda x: 'root@{}.chaosdorf.de'.format(x), 'backend dashbaord extern intern shells vm'.split()))
 
-if not env.hosts:
-    env.hosts = [
-        'backend.chaosdorf.de',
-        'extern.chaosdorf.de',
-        'intern.chaosdorf.de',
-        'shells.chaosdorf.de',
-        'vm.chaosdorf.de',
-    ]
+# Git helpers
 
-env.shell = '/bin/sh -c'
+def etckeeper_check(c):
+    c.run('etckeeper pre-install')
 
-def etckeeper_check():
-    sudo('etckeeper pre-install')
+def etckeeper_commit(c, message):
+    c.run('''if etckeeper unclean; then etckeeper commit '{}'; fi'''.format(message))
 
-def etckeeper_commit(message):
-    sudo('if etckeeper unclean; then etckeeper commit "%s"; fi' % message)
+def etckeeper_done(c):
+    c.run('etckeeper post-install')
 
-def etckeeper_done():
-    sudo('etckeeper post-install')
+def usrlocal_check(c):
+    c.run('cd /usr/local && test -z "`git status --porcelain`"')
 
-def usrlocal_check():
-    with cd('/usr/local'):
-        sudo('test -z "`git status --porcelain`"')
+def usrlocal_commit(c, message):
+    c.run('''if test -n "`git status --porcelain`"; then git add -A .; git commit -m '{}'; fi'''.format(message))
 
-def usrlocal_commit(message):
-    with cd('/usr/local'):
-        sudo('if test -n "`git status --porcelain`"; then git add -A .; git commit -m "%s"; fi' % message)
+# Checks
 
-def configs():
-    etckeeper_check()
-    put('etckeeper/etckeeper.conf', '/etc/etckeeper/', use_sudo=True)
-    put('apt/99checkrestart', '/etc/apt/apt.conf.d/', use_sudo=True)
-    etckeeper_commit('chaosdorf-admin-toolkit configfile updates')
+@task(hosts=all_hosts)
+def test(c):
+    c.run('uptime')
 
-def deploy(version):
-    etckeeper_check()
-    put("../chaosdorf-admin-toolkit_%s_all.deb" % version, '/root/', use_sudo=True)
-    sudo("dpkg --install /root/chaosdorf-admin-toolkit_%s_all.deb" % version)
-    sudo("rm /root/chaosdorf-admin-toolkit_%s_all.deb" % version)
-    etckeeper_done()
+@task(hosts=all_hosts)
+def checkrestart(c):
+    c.run('checkrestart')
 
-def test():
-    sudo("uptime")
+# Upgrade cgit
 
+@task(hosts=['root@intern.chaosdorf.de'])
+def cgit(c):
+    c.run('if ! test -e /opt/cgit/src; then apt --quiet --assume-yes install build-essential libssl-dev zlib1g-dev; mkdir -p /opt/cgit/static; git clone --recursive https://git.zx2c4.com/cgit /opt/cgit/src; fi')
+    with c.cd('/opt/cgit/src'):
+        c.run('git pull')
+        c.run('git submodule update --init')
+        c.run('make')
+        c.run('cp cgit /opt/cgit/cgit.cgi')
+        c.run('cp cgit.css cgit.png /opt/cgit/static')
 
-def upgrade():
-    sudo("apt-get dist-upgrade --quiet")
-    
-def checkrestart():
-    sudo("checkrestart")
+# Add user
 
-@hosts('backend.chaosdorf.de')
-def ldapuser(user_name, first_name, last_name):
-    sudo('cpu useradd -f "%s" -E "%s" -e %s@chaosdorf.de %s' % (first_name, last_name, user_name, user_name))
-    sudo('ldappasswd -y /root/ldap_password -x -W -D cn=admin,dc=chaosdorf,dc=de uid=%s,ou=People,dc=chaosdorf,dc=de' % user_name)
+@task(hosts=['root@backend.chaosdorf.de'])
+def ldapuser(c, user_name, first_name, last_name):
+    c.run('''cpu useradd -f '{first_name}' -E '{last_name}' -e '{user_name}@chaosdorf.de' '{user_name}' '''.format(user_name=user_name, first_name=first_name, last_name=last_name))
+    ldappasswd(c, user_name)
 
-@hosts('backend.chaosdorf.de')
-def ldappasswd(user_name):
-    sudo('ldappasswd -y /root/ldap_password -x -W -D cn=admin,dc=chaosdorf,dc=de uid=%s,ou=People,dc=chaosdorf,dc=de' % user_name)
+@task(hosts=['root@backend.chaosdorf.de'])
+def ldappasswd(c, user_name):
+    c.run('''ldappasswd -y /root/ldap_password -x -W -D cn=admin,dc=chaosdorf,dc=de uid='{user_name}',ou=People,dc=chaosdorf,dc=de'''.format(user_name=user_name))
 
-@hosts('intern.chaosdorf.de')
-def maildir(user_name):
-    sudo('mkdir -p /srv/mail/%s' % user_name)
-    sudo('chown -R %s:%s /srv/mail/%s' % (user_name, user_name, user_name))
+@task(hosts=['root@intern.chaosdorf.de'])
+def maildir(c, user_name):
+    c.run('''mkdir -p '/srv/mail/{user_name}' '''.format(user_name=user_name))
+    c.run('''chown -R '{user_name}:{user_name}' '/srv/mail/{user_name}' '''.format(user_name=user_name))
 
-# by default, fabric runs the user target for each host, even though
-# ldapuser and maildir only work for one. Limiting it to one host fixes this.
-@hosts('backend.chaosdorf.de')
-def user(user_name, first_name, last_name):
-    execute(ldapuser, user_name, first_name, last_name)
-    execute(maildir, user_name)
+@task
+def user(c, user_name, first_name, last_name):
+    with Connection('root@backend.chaosdorf.de') as c_backend:
+        ldapuser(c_backend, user_name, first_name, last_name)
 
-@hosts('backend.chaosdorf.de')
-def mailforward(user_name, mail_forward):
-    ldif = '''dn: uid=%s,ou=People,dc=chaosdorf,dc=de
+    with Connection('root@intern.chaosdorf.de') as c_intern:
+        maildir(c_intern, user_name)
+
+# Set external Mail Forwarding address
+
+@task(hosts=['root@backend.chaosdorf.de'])
+def mailforward(c, user_name, mail_forward):
+    ldif = '''dn: uid={user_name},ou=People,dc=chaosdorf,dc=de
 changetype: modify
 replace: mailRoutingAddress
-mailRoutingAddress: %s
-''' % (user_name, mail_forward)
-    put(StringIO(ldif), 'fabfile_aliases.ldif')
-    sudo('ldapmodify -y /root/ldap_password -x -W -D cn=admin,dc=chaosdorf,dc=de -f fabfile_aliases.ldif')
+mailRoutingAddress: {mail_forward}
+'''.format(user_name=user_name, mail_forward=mail_forward)
+    c.put(io.StringIO(ldif), 'fabfile_aliases.ldif')
+    c.run('ldapmodify -y /root/ldap_password -x -W -D cn=admin,dc=chaosdorf,dc=de -f fabfile_aliases.ldif')
+    c.run('rm fabfile_aliases.ldif')
 
-@hosts('backend.chaosdorf.de')
-def sshkey(user_name, key_file):
-    key = open(key_file).read().replace("\n", '').replace("\r", '')
-    ldif = '''dn: uid=%s,ou=People,dc=chaosdorf,dc=de
-changetype: modify
-add: sshPublicKey
-sshPublicKey: %s
-''' % (user_name, key)
-    put(StringIO(ldif), 'fabfile_keys.ldif')
-    sudo('ldapmodify -y /root/ldap_password -x -W -D cn=admin,dc=chaosdorf,dc=de -f fabfile_keys.ldif')
+# Add Jabber account
 
-@hosts('backend.chaosdorf.de')
-def deluser(user_name):
-    execute(archive_maildir, user_name)
-    execute(archive_home, user_name)
+@task(hosts=['root@extern.chaosdorf.de'])
+def jabber_adduser(c, user_name):
+    print('Temporaeres Passwort: ', end='')
+    c.run('pwgen -s 23 1')
+    c.run('''sudo -u prosody prosodyctl adduser '{user_name}@chaosdorf.de' '''.format(user_name=user_name))
 
-@hosts('intern.chaosdorf.de')
-def archive_maildir(user_name):
-    with cd('/srv/mail'):
-        if exists(user_name):
-            sudo('tar czf {user}.tgz {user}'.format(user = user_name))
-            sudo('rm -r {user}'.format(user = user_name))
+# Delete user
 
-@hosts('extern.chaosdorf.de', 'shells.chaosdorf.de')
-def archive_home(user_name):
-    with cd('/home'):
-        if exists(user_name):
-            sudo('tar czf {user}.tgz {user}'.format(user = user_name))
-            sudo('rm -r {user}'.format(user = user_name))
+@task(hosts=['root@backend.chaosdorf.de'])
+def ldapdeluser(c, user):
+    c.run('''cpu userdel '{user}' && cpu groupdel '{user}' '''.format(user=user))
 
-@hosts('extern.chaosdorf.de')
-def upgrade_mediawiki(version):
-    with cd('/root'):
-        sudo('bash /root/sqldump.sh > /root/mediawiki_before_upgrade_%s.sql' % version)
-    with cd('/srv/www'):
-        sudo('wget --quiet -O mediawiki.tar.gz http://releases.wikimedia.org/mediawiki/%s/mediawiki-%s.tar.gz' 
-             % ('.'.join(version.split('.')[0:2]), version))
-        sudo('tar xf mediawiki.tar.gz -C /srv/www/de.chaosdorf.wiki --strip-components=1')
-        sudo('rm mediawiki.tar.gz')
-        with cd('de.chaosdorf.wiki'):
-            with cd('maintenance'):
-                sudo('php update.php')
-            sudo('git add -A')
-            sudo('git commit -m "Upgrade auf MediaWiki %s"' % version)
+@task(hosts=['root@intern.chaosdorf.de'])
+def archive_maildir(c, user):
+    c.run('''if test -d '/srv/mail/{user}'; then cd /srv/mail && tar czf '{user}.tgz' '{user}' && chmod 600 '{user}.tgz' && rm -r '{user}'; fi'''.format(user=user))
 
-@hosts('extern.chaosdorf.de')
-def deploy_raumstatus():
-    etckeeper_check()
-    usrlocal_check()
-    put('raumstatus/raumstatus_update', '/usr/local/bin', use_sudo=True, mode=0755)
-    put('raumstatus/bilder/*.png', '/srv/www/de.chaosdorf/raumstatus', use_sudo=True)
-    etckeeper_commit('raumstatus update')
-    usrlocal_commit('raumstatus update')
+@task(hosts=['root@extern.chaosdorf.de', 'root@shells.chaosdorf.de'])
+def archive_home(c, user):
+    c.run('''if test -d '/home/{user}'; then cd /home && tar czf '{user}.tgz' '{user}' && chmod 600 '{user}.tgz' && rm -r '{user}'; fi'''.format(user=user))
 
-@hosts('intern.chaosdorf.de')
-def cgit():
-    if exists('/opt/cgit/src'):
-        with cd('/opt/cgit/src') as d:
-            sudo('git pull')
-            sudo('git submodule update --init')
-    else:
-        sudo('apt-get install --assume-yes build-essential libssl-dev zlib1g-dev')
-        sudo('mkdir -p /opt/cgit/static')
-        sudo('git clone --recursive http://git.zx2c4.com/cgit '
-                '/opt/cgit/src')
-
-    with cd('/opt/cgit/src') as d:
-        sudo('make')
-        sudo('cp cgit /opt/cgit/cgit.cgi')
-        sudo('cp cgit.css cgit.png /opt/cgit/static')
-
-# fuer embedded hosts mit readonly / (z.B. raspis im Clubraum)
-def setup_readonly_host():
-    run('mount -o remount,rw /')
-    with cd('/var/lib'):
-        run('if ! readlink logrotate; then rm -rf logrotate; ln -s /tmp logrotate; fi')
-    run('mount -o remount,ro /')
-
-@hosts('extern.chaosdorf.de')
-def jabber_adduser(nickname):
-    local('echo generating temporary password:')
-    local('pwgen -s 23 1')
-    sudo('prosodyctl adduser %s@chaosdorf.de' % nickname, user='prosody')
+@task
+def deluser(c, user_name):
+    with Connection('root@backend.chaosdorf.de') as c_backend:
+        ldapdeluser(c_backend, user_name)
+    with Connection('root@intern.chaosdorf.de') as c_intern:
+        archive_maildir(c_intern, user_name)
+    with Connection('root@extern.chaosdorf.de') as c_extern:
+        archive_home(c_extern, user_name)
+    with Connection('root@shell.chaosdorf.de') as c_shells:
+        archive_home(c_shells, user_name)
